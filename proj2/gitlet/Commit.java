eecd4b212f177a4c92c2fb4edfaf8a7036148e90
anna-and-banana <ripitup.anna@gmail.com>
2024-09-26 17:01:57 +0800

    Created constructor for 'initial commit'

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,24 +2,35 @@ package gitlet;
 
 // TODO: any imports you need here
 
-import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.Date;
 
-/** Represents a gitlet commit object.
+/**
  *  TODO: It's a good idea to give a description here of what else this Class does at a high level.
- *
- *  @author TODO
+ *  Overview: Represents a gitlet commit object.
+ *  Description:
+ *  @author Anna
  */
 public class Commit {
     /**
-     * TODO: add instance variables here.
      *
      * List all instance variables of the Commit class here with a useful
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided one example for `message`.
      */
 
-    /** The message of this Commit. */
-    private String message;
+    // The message of this Commit.
+    private String msg;
+    // The timestamp of this Commit.
+    private Date timestamp;
+    // The parent of this Commit.
+    private Commit parent;
+    // Some other stuff: The Blobs reference...
 
-    /* TODO: fill in the rest of this class. */
+
+    // Initial Commit.
+    public Commit() {
+        this.msg = "initial commit";
+        this.timestamp = new Date(0);
+        this.parent = null;
+    }
 }
